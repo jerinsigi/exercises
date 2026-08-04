7317782099ef0d9c353b03d8649872c4b287f596
Jerin <jerinsigi01@gmail.com>
2026-08-04 06:06:47 +0530

    add function to validate all properties in a collection

---
+++ javascript/allTruePropertyValidator.js
@@ -0,0 +1,9 @@
+function truthCheck(collection, pre) {
+  return collection.every(item => item[pre]);
+}
+
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
+
+
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"))
